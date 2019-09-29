8d20e2a9e6e5db95f60cadb4bdbc9c0e28c7ef4d
kanoshinnosuke <yushph@gmail.com>
2019-09-29 16:05:59 +0800

    add atoms - Navbar, StyledLink

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Root, Routes, addPrefetchExcludes } from 'react-static'
 //
 import { Link, Router } from 'components/Router'
 import Dynamic from 'containers/Dynamic'
-
+//
 import './app.css'
+//
+import Navbar from './components/atoms/Navbar'
+import StyledLink from './components/atoms/StyledLink'
+
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
@@ -12,12 +16,12 @@ addPrefetchExcludes(['dynamic'])
 function App() {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
+      <Navbar>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/blog">Blog</StyledLink>
+        <StyledLink to="/dynamic">Dynamic</StyledLink>
+      </Navbar>
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
---
+++ src/components/atoms/StyledLink.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'components/Router'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+    padding-right: 1rem;
+`;
+
+export default StyledLink;
